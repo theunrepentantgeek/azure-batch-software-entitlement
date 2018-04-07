a2a6f4825a30922af24ca86b80c4e2c240c83a2a
Bevan Arps <bearps@microsoft.com>
2018-04-08 10:28:21 +1200

    Fix sesclient.ps1 search for sesclient

--- sesclient.ps1
+++ sesclient.ps1
@@ -5,7 +5,7 @@
 # Find the latest version of sesclient.native.exe that was compiled
 # [This will automatically find the right version, even if someone does a release 
 # build or if the output path changes]
-$sesclient = get-childitem $PSScriptRoot\x64\sesclient.native.exe -recurse | sort-object LastWriteTimeUtc | select-object -last 1
+$sesclient = get-childitem $PSScriptRoot\sesclient.native.exe -recurse | sort-object LastWriteTimeUtc | select-object -last 1
 
 if ($sesclient -eq $null) 
 {
